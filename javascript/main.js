58cf67a0e237d695471cba0648233cbfc6e4283d
FelipeGalleguillos <f.i.galleguillos@gmail.com>
2022-11-15 19:13:51 -0300

    index con vue

--- javascript/main.js
+++ javascript/main.js
@@ -1,137 +1,51 @@
-const cardCont = document.getElementById('card-container');
-const fragment = document.createDocumentFragment();
-const checkCont = document.getElementById('chkCont');
-const searchBar = document.getElementById('searchSection');
-let events;
-fetch("https://amazing-events.herokuapp.com/api/events")
-    .then(element => element.json())
-    .then(array => {
-        events = array.events;
-        checkCategories(events)
-        renderCards(textFilter(cardsFiltered(events, filteredCat), searchBar.value.toLowerCase().trim()))
-    });
-    
-let filteredCat = []
-////////////////////////////////////////////////////////////////////////////
-function checkCategories(events) {
-    let aux
-    aux = events.map(element => {
-        return element.category
-    })
-    for (let i = 0; i < aux.length; i++) {
-        for (let x = 0; x < aux.length; x++) {
-            if (aux[i] == aux[x]) {
-                aux.splice(x, 1)
-            }
-        }
-    }
-    let categories = aux
-    categories.forEach(element => {
-        checkCont.innerHTML += `<div>
-                                <input type="checkbox" id="${element}" name="" value="${element}">
-                                <label class="checks" for="${element}">${element}</label>
-                                </div>
-        `
-    });
-}
-////////////////////////////////////////////////////////////////////////////
-function addCategory(category) {
-    filteredCat.push(category)
-}
-////////////////////////////////////////////////////////////////////////////
-function deleteCategory(category) {
-    filteredCat.forEach(element => {
-        if (element == category) {
-            filteredCat.splice(filteredCat.indexOf(element), 1)
-        }
-    })
-}
-///////////////////////////////////////////////////////////////////////////
-function textFilter(array, text) {
-    if (text.length == 0) {
-        return array
-    } else {
-        return array.filter(element => element.name.toLowerCase().includes(text) || element.description.toLowerCase().includes(text))
-    }
-}
-//////////////////////////////////////////////////////////////////////////////
+const { createApp } = Vue
 
-function cardsFiltered(events, categories) { //returns array with objects filtered by category
-    if (categories.length == 0) {
-        return events
-    } else {
-        let aux1 = []
-        let aux2, aux3
-        categories.forEach(category => {
-            aux3 = events.filter(element => element.category == category)
-            aux1 = aux1.concat(aux3)
+const app = createApp({
+    data(){
+        return{
+            events:[],
+            checked:[],
+            categories:[],
+            date:[],
+            checked:[],
+            filteredEvents:[],
+            searchInput:''
+
+        }
+
+    },
+    created(){
+        fetch("https://amazing-events.herokuapp.com/api/events")
+        .then(res=>res.json())
+        .then(array=>{
+            this.events=array.events
+            this.filteredEvents=array.events
+            this.date=array.currentDate.split('-')
+            this.getCategories()
         })
-        aux2 = Array.from(new Set(aux1))
-        return aux2
-    }
-}
-///////////////////////////////////////////////////////////////////////////////
-function renderCards(events) {
-    console.log(events)
-    cardCont.innerHTML = ""
-    if (events.length == 0) {
-        cardCont.innerHTML = `<h3>No events to show....</h3>`
-    } else {
+        .catch(err=>console.log(err))
+    },
+    methods:{
+        getCategories(){
+            let fn = event => event.category 
+            this.categories=[... new Set(this.events.filter(fn).map(fn))]   
+        },
 
-        for (let cardInfo of events) {
-            const card = document.createElement('div');
-            card.classList.add('card', 'text-bg-danger', 'cardAnim');
-            card.style.width = '18rem';
-            card.style.height = '25rem';
-            card.innerHTML = `<img src="${cardInfo.image}"class="card-img-top h-50" alt="..."></img>
-                <div class="card-body">
-                    <h5 class="card-title">${cardInfo.name}</h5>
-                    <p class="card-text">${cardInfo.description}</p>
-                </div>
-                <div class="card-footer">
-                    <div class="d-flex justify-content-between">
-                        <p>$${cardInfo.price}</p>
-                        <a id="" href="./details.html?id=${cardInfo._id}" class="btn btn-primary">See more</a>
-                    </div>
-                </div>
-                `
-            fragment.appendChild(card);
-        }
-        cardCont.appendChild(fragment);
-    }
-}
+    },
+    computed:{
+        filter(){
+            const firstFilter = this.events.filter(event=>this.checked.includes(event.category))
+            const secondFilter = firstFilter.filter(event=>event.name.toLowerCase().trim().includes(this.searchInput.toLowerCase().trim()))
+            if(firstFilter.length==0){
+                this.filteredEvents=this.events.filter(event=>event.name.toLowerCase().trim().includes(this.searchInput.toLowerCase().trim()))
+            }else{
+                this.filteredEvents=secondFilter
+            }
 
-//////////////////////////////////////////////////////////////////////////////////////////////
-checkCont.addEventListener("click", (e) => {
-    if (e.target.checked) {
-        addCategory(e.target.value)
-        renderCards(textFilter(cardsFiltered(events, filteredCat), searchBar.value.toLowerCase().trim()))
-    } else {
-        deleteCategory(e.target.value)
-        renderCards(textFilter(cardsFiltered(events, filteredCat), searchBar.value.toLowerCase().trim()))
-    }
-})
-/////////////////////////////////////////////////////////////////////////////////
-searchBar.addEventListener("keyup", () => {
-    renderCards(textFilter(cardsFiltered(events, filteredCat), searchBar.value.toLowerCase().trim()))
+        },
+    },
+
 })
 
-// cardCont.addEventListener("click",(e)=>{
+app.mount('#app')
 
-// })
-
-
-
-/* <div class="card h-100 text-bg-danger" style="width: 15rem;">
-    <img src="./assets/Feria_de_comidas7.jpg" class="card-img-top" alt="..."></img>
-        <div class="card-body">
-            <h5 class="card-title">Festival of the colectivities</h5>
-            <p class="card-text">Enjoy your favorite dishes from different countries in a unique event for the whole family.</p>
-        </div>
-        <div class="card-footer">
-            <div class="d-flex justify-content-between">
-                <p>$5</p>
-                <a href="#" class="btn btn-primary">See more</a>
-            </div>
-        </div>
-</div> */
